8c771c690ba622945594603835c3aa673c8c2622
sko6455008 <koudai645508@gmail.com>
2025-06-24 15:09:12 +0900

    料金と募集要項の修正

--- wp-content/themes/twelor/recruit.php
+++ wp-content/themes/twelor/recruit.php
@@ -8,11 +8,6 @@ get_header();
 <div class="container py-5">
     <h1 class="recruit-section-title"><?php echo esc_html(get_the_title()); ?></h1>
 
-    <div class="col-12 position-relative">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/recruit_bg.jpg" alt="求人募集背景" class="img-fluid recruit-bg">
-        <img src="<?php echo get_template_directory_uri(); ?>/assets/images/openingstaff.png" alt="求人募集" class="recruit">
-    </div>
-
     <div class="requirements-table-container">
         <table class="requirements-table">
             <tbody>
@@ -35,31 +30,53 @@ get_header();
                     <td class="table-content">
                         <div class="salary-details">
                             <div>
-                                <p><strong>【正社員】月給250,000円以上</strong></p>
-                                <p>●例　実働8.5/休憩1.5<br>
-                                    250.440円　月24日出勤<br>
-                                    240.000円　月23日出勤<br>
-                                    229.500円　月22日出勤<br>
-                                    219.100円　月21日出勤<br>
-                                    208.600円　月20日出勤<br>
+                                <p><strong>【正社員】月給250,000～300,000円</strong></p>
+                                <p>時短正社員要相談♪</p>
+                                <p>
+                                    ▷経験や能力、前職の実績により給与は考慮致します。<br>
+                                    <strong>※1年以上の経験者</strong>
+                                </p>
+                                <p>▽給与内訳-例<br>
+                                    基本給　　240,000円<br>
+                                    固定残業　15,000円<br>
+                                    業務手当　5,000円<br>
+                                    皆勤手当　10,000円<br>
+                                    指名20人　10,000円<br>
+                                    交通費　　15,000円<br>
+                                    達成　　　20,000円<br>
+                                    ＝総支給　315,000円<br>
+                                </p>
+                                <p>
+                                    ※朝の着替え準備、夜の着替え、帰宅準備他固定残業代含む<br>
+                                    ※取得していない技術はサロンで技術指導もしています<br>
+                                    └しっかりとした研修制度があるので、フォロー体制はばっちりです！<br>
+                                    ※朝の着替え準備、夜の着替え、帰宅準備他、残業分は別途支給
+                                </p>
+                                <p>
+                                    ☆固定残業代は残業の有無に関わらず支給<br>
+                                    ☆業務時間内の技術研修あり<br>
+                                    ☆指名料100％還元♪<br>
                                 </p>
                             </div>
                             
                             <div>
-                                <p><strong>【アルバイト・パート】時給1,100円～1,300円</strong></p>
+                                <p><strong>【アルバイト・パート】時給1,300円～1,500円</strong></p>
+                                <p>時短正社員要相談♪</p>
+                                <p>▷経験や能力、前職の実績により給与は考慮致します。<br>※1年以上の経験者</p>
+                                <p>☆交通費支給</p>
                                 <p>
-                                    ※土日祝や夜の時間も出勤可能な方<br>
-                                    勤務初日から同時給が嬉しい♪<br>
-                                    研修中も安心して勤めることができます！<br>
+                                    ※取得していない技術はサロンで技術指導もしています<br>
+                                    └しっかりとした研修制度があるので、フォロー体制はばっちりです！<br>
+                                    ※朝の着替え準備、夜の着替え、帰宅準備他、残業分は別途支給
                                 </p>
                             </div>
                             
                             <div>
                                 <p><strong>【業務委託】 完全歩合</strong></p>
                                 <p>
-                                    ※完全歩合制　売上の50％<br>
-                                    ※経験・能力を考慮し、優遇いたします。<br>
+                                    ※売上歩合率：50％～(保証給あり)<br>
                                     ※1日3～4時間勤務より応相談<br>
+                                    ※経験・能力を考慮し、優遇いたします
                                 </p>
                             </div>
                         </div>
@@ -68,20 +85,25 @@ get_header();
                 <tr>
                     <th class="table-header">勤務地</th>
                     <td class="table-content">
-                        <p>東京都豊島区池袋2-40-13 VORT1池袋ビル3F</p>
+                        <p>東京都豊島区西池袋5-2-3 平凡立教前ビル6F</p>
                     </td>
                 </tr>
                 <tr>
                     <th class="table-header">勤務時間</th>
                     <td class="table-content">
                         <div>
-                            <p><strong>【正社員】</strong><br>
-                                平日:11:00～23:00(のうちシフト制)<br>
-                                土曜・日曜・祭日:10:00～23:00(のうちシフト制)
+                            <p>
+                                平日　:11:00～20:00<br>
+                                　　　:12:00～21:00<br>
+                                土日祝:10:00～19:00<br>
+                                　　　:11:00～20:00<br>
                             </p>
-                            <p><strong>【アルバイト】</strong><br>
-                                週2日、1日5時間～
+                            <p>
+                                上記でシフト制<br>
+                                実働8時間*休憩1時間<br>
+                                残業ほぼなし♪
                             </p>
+                            <p>時短勤務相談ください</p>
                         </div>
                     </td>
                 </tr>
@@ -89,22 +111,19 @@ get_header();
                     <th class="table-header">休日</th>
                     <td class="table-content">
                         <div>
-                            <p><strong>【正社員】</strong>
                             <ul>
-                                <li>週休2日</li>
-                                <li>有給休暇</li>
-                                <li>産休・育休</li>
+                                <li>出勤日数相談</li>
+                                <li>土日祝お休みOK</li>
+                                <li>連休OK</li>
+                                <li>有給取得率100%</li>
+                                <li>年末年始休暇</li>
+                                <li>産休・育休制度あり♪</li>
+                                <li>固定休み希望もOK</li>
                             </ul>
-                            <p><strong>【アルバイト】</strong></p>
-                            <ul>
-                                <li>希望休</li>
-                                <li>有給休暇</i>
-                                <li>産休・育休</li>
-                            </ul>
-                            <p><strong>【業務委託】</strong></p>
-                            <ul>
-                                <li>応相談</li>
-                            </ul>
+                            <p>
+                                週３休制ですが、稼ぎたい方はどんどん出勤OKです！<br>
+                                働き方は貴方に合ったスタイルで柔軟に対応致します♪
+                            </p>
                         </div>
                     </td>
                 </tr>
@@ -114,29 +133,45 @@ get_header();
                         <div>
                             <p><strong>【共通】</strong></p>
                             <ul>
+                                <li>ノルマ・残業・ミーティングなし</li>
+                                <li>ロッカー完備</li>
+                                <li>髪色・髪型自由</li>
+                                <li>服装自由</li>
                                 <li>制服貸与</li>
                             </ul>
                             <p><strong>【正社員】</strong></p>
                             <ul>
-                                <li>交通費支給(10,000円/月迄)</li>
-                                <li>有給休暇(勤務半年後から10日間発生)</li>
-                                <li>産休・育休</li>
-                                <li>社会保険(健康保険・厚生年金・雇用保険・労災加入)</li>
-                                <li>決算賞与/年1回(年度業績に応じて)</li>
-                                <li>健康診断</li>
+                                <li>交通費全額支給</li>
+                                <li>指名料100%バック</li>
+                                <li>売上歩合</li>
+                                <li>資格手当</li>
+                                <li>役職手当</li>
+                                <li>有給あり(入社6ヵ月～)</li>
+                                <li>社会保険</li>
+                                <li>雇用保険</li>
+                                <li>労災保険</li>
+                                <li>産休育休実績あり</li>
+                                <li>決算賞与あり(業績に応じて)</li>
+                                <li>皆勤手当て</li>
                             </ul>
                             <p><strong>【アルバイト】</strong></p>
                             <ul>
-                                <li>交通費支給(10,000円/月迄)</li>
-                                <li>有給休暇(勤務半年後から発生)</li>
-                                <li>社会保険・厚生年金・雇用保険・労災加入(勤務時間・日数によって)</li>
-                                <li>決算賞与/年1回(年度業績に応じて)</li>
-                                <li>健康診断(勤務時間・日数によって)</li>
+                                <li>交通費全額支給</li>
+                                <li>指名料100%バック</li>
+                                <li>資格手当</li>
+                                <li>役職手当</li>
+                                <li>有給あり(入社6ヵ月～)</li>
+                                <li>雇用保険</li>
+                                <li>労災保険</li>
                             </ul>
                             <p><strong>【業務委託】</strong></p>
                             <ul>
-                                <li>最低保障給</li>
+                                <li>社員登用あり</li>
                             </ul>
+                            <p>
+                                働き方は人それぞれ、自分に合ったスタイルを♪<br>
+                                楽しく長く働いて、安定してしっかり稼ぐ事で、あなたの未来が変わってくるはずです！
+                            </p>
                         </div>
                     </td>
                 </tr>
@@ -145,13 +180,20 @@ get_header();
                     <td class="table-content">
                         <div>
                             <ul>
-                                <li><strong>1年以上の実務経験ある方</strong></li>
-                                <li>幹部候補・店長候補も募集中!!</li>
-                                <li>夕方からの勤務ができる方歓迎</li>
-                                <li>土曜・日曜・祭日勤務できる方優遇</li>
-                                <li>【アルバイト】は週2日、1日5時間～面接時に相談</li>
-                                <li>20歳～35歳</li>
+                                <li>「好き」を仕事にしたい方</li>
+                                <li>たくさん経験を積みたい</li>
+                                <li>お客様に合わせた気配りができる</li>
+                                <li>丁寧な接客ができる</li>
+                                <li>お客様のお話を聞きながら施術できる</li>
+                                <li>「資格不問」「年齢不問」伸び伸びと今までのキャリアを生かしたい</li>
+                                <li>ブランクがあるけどまたネイルしたい</li>
+                                <li>土日出勤可能な方</li>
                             </ul>
+                            <p>とにかく弊社のビジョンに共感し、お店を盛り上げてくれる方！</p>
+                            <p>
+                                サロンワーク経験者ならブランクも歴浅さんも大歓迎です♪<br>
+                                未経験者も大歓迎です！
+                            </p>
                         </div>
                     </td>
                 </tr>
@@ -165,7 +207,7 @@ get_header();
         text-align: center;
         color: #333;
         font-size: 24px;
-        margin: 30px auto;
+        margin: 30px auto 70px;
         padding-bottom: 10px;
         width: 100%;
         border-bottom: 1px solid #ddd;
@@ -200,28 +242,10 @@ get_header();
         border-bottom: none;
     }
 
-    .recruit-bg {
-        height: 350px;
-        width: 100%;
-    }
-
-    .recruit {
-        position: absolute;
-        top: 20%;
-        left: 15%;
-        transform: translate(-50%, -25%);
-        width: 300px;
-        height: 300px;
-        z-index: 10;
-    }
-
     @media (max-width: 768px) {
         .table-header {
             padding: 20px 0px;
             width: 50px;
-        }
-        .recruit {
-            left: 25%;
         }
     }
 </style>
